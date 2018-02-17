8f935b2e80f591fad10bedd6dc3d6caf433fddb3
CeesarL2002 <ceesarleon2002@gmail.com>
2018-02-16 21:59:15 -0400

    El personaje tiene gravedad y salto

--- Game/Actors/Player/Player.gd
+++ Game/Actors/Player/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 var in_game = true
 
 var direction = 1
-var speed = 50
+var speed = 70
 var jump_speed = 100
-var gravity = 60
+var gravity = 200
 
 var velocity = Vector2()
 var distance = Vector2()
@@ -20,6 +20,12 @@ func _physics_process(delta):
 func _move(delta):
 	distance.x = speed*delta
 	velocity.x = (distance.x*direction)/delta
-	velocity.y = 0
+	velocity.y += gravity*delta 
 	
-	move_and_slide(velocity)
+	move_and_slide(velocity, Vector2(0,-1))
+	
+	if is_on_floor():
+		velocity.y = 0
+		
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_speed
