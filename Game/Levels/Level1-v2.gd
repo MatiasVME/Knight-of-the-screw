b235da78b3db979670e7f822e88931215e9ae7ea
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-02-17 16:22:20 -0300

    Ahora hay techo

--- Game/Levels/Level1-v2.gd
+++ Game/Levels/Level1-v2.gd
@@ -3,28 +3,36 @@
 extends Node2D
 
 var rec_block = load("res://Game/Actors/Blocks/Brick.tscn")
+#var rec_enemy_object = load("res://Game/Actors/Enemies/EnemyObject.tscn")
+
 var blocks_floor_increment = 1
+var blocks_roof_increment = 1
 
 var player
 var camera
 
 func _ready():
 	player = PlayerManager.player
-	player.position = Vector2(600, -100)
+	player.position = Vector2(600, 100)
 	
 	camera = Camera2D.new()
 	camera.offset.x = 400
-	camera.offset.y = -100
+	camera.offset.y = -70
 	camera.current = true
-	camera.limit_top = -50
+	camera.limit_top = 0
 	camera.limit_smoothed = true
 	camera.smoothing_enabled = true
 	
+#	var ins_enemy_objects = rec_enemy_object.instance()
+#	ins_enemy_objects.global_position.x = camera.global_position.x
+	
+	camera.add_child(ins_enemy_objects)
 	player.add_child(camera)
 	add_child(player)
 	
 func _physics_process(delta):
 	build_floor()
+	build_roof()
 
 func build_floor():
 	if player.position.x + Main.RES_X * blocks_floor_increment:
@@ -32,3 +40,10 @@ func build_floor():
 		ins_block.position = Vector2(blocks_floor_increment * 64 * 1 + 1, Main.RES_Y - 32)
 		add_child(ins_block)
 		blocks_floor_increment += 1
+
+func build_roof():
+	if player.position.x + Main.RES_X * blocks_roof_increment:
+		var ins_block = rec_block.instance()
+		ins_block.position = Vector2(blocks_roof_increment * 64 * 1 + 1, 0)
+		add_child(ins_block)
+		blocks_roof_increment += 1
